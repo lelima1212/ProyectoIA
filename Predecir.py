a0c6a29e2eb5cc5d08c82dd9765cb750f0efe776
llimap <lejalima@espol.edu.ec>
2020-08-31 22:37:52 -0500

    Despues de entrenamiento

--- Predecir.py
+++ Predecir.py
@@ -1,28 +1,3 @@
-import numpy as np
-from keras.preprocessing.image import load_img, img_to_array
-from keras.models import load_model
-
-longitud, altura=100, 100
-modelo='./modelo/modelo.h5'
-pesos ='./modelo/pesos.h5'
-cnn = load_model(modelo)
-cnn.load_weights(pesos)
-
-def predict(file):
-    x=load_img(file, target_size=(longitud,altura))
-    x=img_to_array(x)
-    x=np.expand_dims(x, axis=0)
-    arreglo=cnn.predict(x) ##[[1,0,0]] 1 donde piense que es correcto
-    resultado=arreglo[0]
-    respuesta=np.argmax(resultado)
-    if respuesta==0:
-        print('Cavendish')
-    elif respuesta==1:
-        print('Orito')
-    elif respuesta==2:
-        print('Rojo')
-    return respuesta
-
-##predict()
-
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:b9a05d45f3686c28a41510f4947da2be1ef26d145dee76d7caf1367533f40024
+size 678
